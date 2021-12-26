ae0b8a6af725ed1d3d9487215782be5d1034ca62
bitsUnxte <96054865+bitsUnxte@users.noreply.github.com>
2021-12-26 11:04:18 +0530

    hhh

--- mailHandler.php
+++ mailHandler.php
@@ -3,14 +3,14 @@
         $to = "aryan.bhosale1719@gmail.com"; 
         $from = $_POST['email']; 
         $name = $_POST['name'];
-        $subject = "Form submission";
+        $subject = $_POST['subject'];
         $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
         $headers = "From: " . $from;
         if(mail($to,$subject,$message,$headers)){
-            echo "Mail Sent. Thank you " . $name . ", I will contact you shortly.";
+            echo "Messages recieved";
         }
         else{
-            echo "Seems like an error occured. Please try again with complete information."
+            echo "Seems like an error occured. Please try again with complete information.";
         }
     }
 ?>
