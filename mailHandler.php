87bc92295c85303afdaaa26af41ac112fac3c6ae
bitsUnxte <96054865+bitsUnxte@users.noreply.github.com>
2021-12-26 13:14:53 +0530

    done!

--- mailHandler.php
+++ mailHandler.php
@@ -1,12 +1,13 @@
 <?php 
-    if(isset($_POST['submit'])){
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $to = "aryan.bhosale1719@gmail.com"; 
         $from = $_POST['email']; 
         $name = $_POST['name'];
         $subject = $_POST['subject'];
         $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
         $headers = "From: " . $from;
-        if(mail($to,$subject,$message,$headers)){
+        mail($to,$subject,$message,$headers);
+        if(mail($to$subject,$message,$headers)){
             echo "Messages recieved";
         }
         else{
